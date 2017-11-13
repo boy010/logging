6a7c364dc35fb536388483fed9c7a96b68a08fdb
FengJian <michael0304@126.com>
2017-11-13 20:16:34 +0800

    add 将日志输出到文件和控制台窗口

--- logging_test.py
+++ logging_test.py
@@ -5,11 +5,32 @@ import logging
 # logging.basicConfig(level=logging.WARNING,  
                     # format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')  
 
-logging.basicConfig(level=logging.WARNING,
-                    filename='./log/log.txt',
-                    filemode='w',
-                    format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
+# logging.basicConfig(level=logging.WARNING,
+#                     filename='./log/log.txt',
+#                     filemode='w',
+#                     format='%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s')
 
+#step 1, create one logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+#Step 2, create one handler, to write log file
+logfile = './log/logger.txt'
+fh = logging.FileHandler(logfile, mode='w')
+fh.setLevel(logging.DEBUG)
+
+#Step 3, create another handler , to output to consle
+ch = logging.StreamHandler()
+ch.setLevel(logging.WARNING)
+
+# Step 4, define handler output format
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+#Step 5, add logger to handler
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 # use logging  
 logging.info('this is a loggging info message')  
